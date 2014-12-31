e218b00500c29efb761dee437f86ffd557fd2c19
mohammadaslam <me.aslam@gmail.com>
2014-12-31 12:47:25 +0530

    second commit

--- HelloWorld/src/mycom/setup/HelloWorld.java
+++ HelloWorld/src/mycom/setup/HelloWorld.java
@@ -2,6 +2,6 @@ package mycom.setup;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("This is the first update.");
+		System.out.println("This is the second update.");
 	}
 }
